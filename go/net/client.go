62efef74cd9d7132c625909596ffe60e12e447c9
sky <sky_8691@qq.com>
2022-12-18 10:54:46 +0800

    新文件：   go/net/client.go 	新文件：   go/net/net.md 	新文件：   go/net/server.go 	删除：     linux/proc/proc.md

---
+++ go/net/client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+var Stdin = bufio.NewReader(os.Stdin)
+
+func Scanln(str *string) (int, error) {
+	s, _, err := Stdin.ReadLine()
+	*str = string(s)
+	return len(s), err
+}
+
+func OnRecived(conn *net.TCPConn) {
+	r := bufio.NewReader(conn)
+	var s string
+	for {
+		Scanln(&s)
+		_, err := conn.Write([]byte(s + "\n"))
+		if err != nil {
+			panic("连接断开")
+		}
+		s, err = r.ReadString('\n')
+		if err != nil {
+			panic("连接断开")
+		}
+		fmt.Println("收到消息：", s)
+	}
+}
+
+func main() {
+	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:8888")
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("连接成功")
+	}
+	defer conn.Close()
+	OnRecived(conn)
+}
