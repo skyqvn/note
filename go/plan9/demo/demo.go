41ab8cd71beab99d0e5428b0a54f74b22174db00
sky <sky_8691@qq.com>
2023-02-11 13:25:44 +0800

    .

--- go/plan9/demo/demo.go
+++ go/plan9/demo/demo.go
@@ -1,7 +1,14 @@
 package main
 
+import "fmt"
+
 func Print()
 
+func Factorial(a int) int
+
 func main() {
+	//DEMO1
 	Print()
+	//DEMO2
+	fmt.Println(Factorial(10))
 }
--- go/plan9/demo/demo_linux_amd64.s
+++ go/plan9/demo/demo_linux_amd64.s
@@ -1,5 +1,6 @@
 #include "textflag.h"
 
+//DEMO1
 DATA ·Data+0(SB)/8,$·Data+16(SB)
 DATA ·Data+8(SB)/8,$12
 DATA ·Data+16(SB)/16,$"hello world\n"
@@ -11,4 +12,25 @@ TEXT  ·Print(SB),NOSPLIT,$0-0
     MOVQ ·Data+0(SB),SI
     MOVQ ·Data+8(SB),DX
     SYSCALL
+    RET
+
+//DEMO2
+TEXT ·Factorial(SB),$0-16
+    SUBQ $24,SP
+    MOVQ BP,16(SP)
+    LEAQ 16(SP),BP
+    MOVQ $1,ret+40(SP)
+    MOVQ a+32(SP),AX
+    MOVQ AX,t+8(SP)
+    begin:
+    CMPQ t+8(SP),$0
+    JLE end
+    MOVQ ret+40(SP),AX
+    IMULQ t+8(SP),AX
+    MOVQ AX,ret+40(SP)
+    DECQ t+8(SP)
+    JMP begin
+    end:
+    MOVQ 16(SP),BP
+    ADDQ $24,SP
     RET
